e9e0764564126f12204cb6ccbc97c98e0ddec41d
Mariusz Gorski <marius.gorski@gmail.com>
2013-03-26 21:16:52 +0100

    Reorder methods in ALU

--- src/main/java/gorskima/z80emu/ALU.java
+++ src/main/java/gorskima/z80emu/ALU.java
@@ -75,11 +75,70 @@ public class ALU {
 		registers.setFlag(Flag.C, adder.isBorrow());
 	}
 
+	public void inc(final Register r) {
+		int op1 = registers.getRegister(r);
+	
+		Adder adder = new Adder();
+		int result = adder.add(op1, 1, 0);
+		registers.setRegister(r, result);
+	
+		registers.setFlag(Flag.S, getSign(result));
+		registers.setFlag(Flag.Z, isZero(result));
+		registers.setFlag(Flag.H, adder.isHalfCarry());
+		registers.setFlag(Flag.PV, adder.isOverflow());
+		registers.setFlag(Flag.N, false);
+		registers.setFlag(Flag.C, adder.isCarry());
+	}
+
+	public void dec(final Register r) {
+		int op1 = registers.getRegister(r);
+	
+		Adder adder = new Adder();
+		int result = adder.sub(op1, 1, 0);
+		registers.setRegister(r, result);
+	
+		registers.setFlag(Flag.S, getSign(result));
+		registers.setFlag(Flag.Z, isZero(result));
+		registers.setFlag(Flag.H, adder.isHalfBorrow());
+		registers.setFlag(Flag.PV, adder.isOverflow());
+		registers.setFlag(Flag.N, true);
+		registers.setFlag(Flag.C, adder.isBorrow());
+	}
+
+	public void cp(final int op2) {
+		int op1 = registers.getRegister(Register.A);
+	
+		Adder adder = new Adder();
+		int result = adder.sub(op1, op2, 0);
+	
+		registers.setFlag(Flag.S, getSign(result));
+		registers.setFlag(Flag.Z, isZero(result));
+		registers.setFlag(Flag.H, adder.isHalfBorrow());
+		registers.setFlag(Flag.PV, adder.isOverflow());
+		registers.setFlag(Flag.N, true);
+		registers.setFlag(Flag.C, adder.isBorrow());
+	}
+
+	public void neg() {
+		int op2 = registers.getRegister(Register.A);
+	
+		Adder adder = new Adder();
+		int result = adder.sub(0, op2, 0);
+		registers.setRegister(Register.A, result);
+	
+		registers.setFlag(Flag.S, getSign(result));
+		registers.setFlag(Flag.Z, isZero(result));
+		registers.setFlag(Flag.H, adder.isHalfBorrow());
+		registers.setFlag(Flag.PV, adder.isOverflow());
+		registers.setFlag(Flag.N, true);
+		registers.setFlag(Flag.C, adder.isBorrow());
+	}
+
 	public void and(final int op2) {
 		int op1 = registers.getRegister(Register.A);
 		int result = (op1 & op2) & 0xFF;
 		registers.setRegister(Register.A, result);
-
+	
 		registers.setFlag(Flag.S, getSign(result));
 		registers.setFlag(Flag.Z, isZero(result));
 		registers.setFlag(Flag.H, true);
@@ -92,7 +151,7 @@ public class ALU {
 		int op1 = registers.getRegister(Register.A);
 		int result = (op1 | op2) & 0xFF;
 		registers.setRegister(Register.A, result);
-
+	
 		registers.setFlag(Flag.S, getSign(result));
 		registers.setFlag(Flag.Z, isZero(result));
 		registers.setFlag(Flag.H, false);
@@ -105,7 +164,7 @@ public class ALU {
 		int op1 = registers.getRegister(Register.A);
 		int result = (op1 ^ op2) & 0xFF;
 		registers.setRegister(Register.A, result);
-
+	
 		registers.setFlag(Flag.S, getSign(result));
 		registers.setFlag(Flag.Z, isZero(result));
 		registers.setFlag(Flag.H, false);
@@ -114,72 +173,14 @@ public class ALU {
 		registers.setFlag(Flag.C, false);
 	}
 
-	public void cp(final int op2) {
-		int op1 = registers.getRegister(Register.A);
-
-		Adder adder = new Adder();
-		int result = adder.sub(op1, op2, 0);
-
-		registers.setFlag(Flag.S, getSign(result));
-		registers.setFlag(Flag.Z, isZero(result));
-		registers.setFlag(Flag.H, adder.isHalfBorrow());
-		registers.setFlag(Flag.PV, adder.isOverflow());
-		registers.setFlag(Flag.N, true);
-		registers.setFlag(Flag.C, adder.isBorrow());
-	}
-
-	public void inc(final Register r) {
-		int op1 = registers.getRegister(r);
-
-		Adder adder = new Adder();
-		int result = adder.add(op1, 1, 0);
-		registers.setRegister(r, result);
-
-		registers.setFlag(Flag.S, getSign(result));
-		registers.setFlag(Flag.Z, isZero(result));
-		registers.setFlag(Flag.H, adder.isHalfCarry());
-		registers.setFlag(Flag.PV, adder.isOverflow());
-		registers.setFlag(Flag.N, false);
-		registers.setFlag(Flag.C, adder.isCarry());
-	}
-
-	public void dec(final Register r) {
-		int op1 = registers.getRegister(r);
-
-		Adder adder = new Adder();
-		int result = adder.sub(op1, 1, 0);
-		registers.setRegister(r, result);
-
-		registers.setFlag(Flag.S, getSign(result));
-		registers.setFlag(Flag.Z, isZero(result));
-		registers.setFlag(Flag.H, adder.isHalfBorrow());
-		registers.setFlag(Flag.PV, adder.isOverflow());
-		registers.setFlag(Flag.N, true);
-		registers.setFlag(Flag.C, adder.isBorrow());
-	}
-
+	// TODO not sure if CPL belongs to ALU, let's leave it here for now
 	public void cpl() {
 		int op = registers.getRegister(Register.A);
 		int result = invert(op);
 		registers.setRegister(Register.A, result);
-
+	
 		registers.setFlag(Flag.H, true);
 		registers.setFlag(Flag.N, true);
-	}
-
-	public void neg() {
-		int op2 = registers.getRegister(Register.A);
-
-		Adder adder = new Adder();
-		int result = adder.sub(0, op2, 0);
-		registers.setRegister(Register.A, result);
-
-		registers.setFlag(Flag.S, getSign(result));
-		registers.setFlag(Flag.Z, isZero(result));
-		registers.setFlag(Flag.H, adder.isHalfBorrow());
-		registers.setFlag(Flag.PV, adder.isOverflow());
-		registers.setFlag(Flag.N, true);
-		registers.setFlag(Flag.C, adder.isBorrow());
 	}
 
 	private boolean getSign(final int op) {
