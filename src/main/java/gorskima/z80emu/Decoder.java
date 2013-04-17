ac4c118091656aa81a0c20bae6102d3d892ad9fa
Mariusz Gorski <marius.gorski@gmail.com>
2013-04-17 19:50:34 +0200

    Make flag and register maps immutable

--- src/main/java/gorskima/z80emu/Decoder.java
+++ src/main/java/gorskima/z80emu/Decoder.java
@@ -1,7 +1,8 @@
 package gorskima.z80emu;
 
-import java.util.HashMap;
 import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
 
 public class Decoder {
 
@@ -9,57 +10,39 @@ public class Decoder {
 		r, dd, ss, qq, pp, rr
 	};
 
-	private Map<RegisterType, Map<Integer, Register>> map;
-	private Map<Integer, Flag> flagMap;
+	private final Map<RegisterType, Map<Integer, Register>> registerMap;
+	private final Map<Integer, Flag> flagMap;
 
 	public Decoder() {
-		map = new HashMap<Decoder.RegisterType, Map<Integer, Register>>();
+		Map<Integer, Register> r = ImmutableMap.<Integer, Register>builder()
+			.put(0, Register.B).put(1, Register.C).put(2, Register.D).put(3, Register.E)
+			.put(4, Register.H).put(5, Register.L).put(7, Register.A)
+			.build();
 
-		Map<Integer, Register> r = new HashMap<Integer, Register>();
-		r.put(0, Register.B);
-		r.put(1, Register.C);
-		r.put(2, Register.D);
-		r.put(3, Register.E);
-		r.put(4, Register.H);
-		r.put(5, Register.L);
-		r.put(7, Register.A);
-		map.put(RegisterType.r, r);
+		Map<Integer, Register> dd = ImmutableMap.<Integer, Register>builder()
+			.put(0, Register.BC).put(1, Register.DE).put(2, Register.HL).put(3, Register.SP)
+			.build();
 
-		Map<Integer, Register> dd = new HashMap<Integer, Register>();
-		dd.put(0, Register.BC);
-		dd.put(1, Register.DE);
-		dd.put(2, Register.HL);
-		dd.put(3, Register.SP);
-		map.put(RegisterType.dd, dd);
+		Map<Integer, Register> qq = ImmutableMap.<Integer, Register>builder()
+			.put(0, Register.BC).put(1, Register.DE).put(2, Register.HL).put(3, Register.AF)
+			.build();
 
-		map.put(RegisterType.ss, dd);
+		Map<Integer, Register> pp = ImmutableMap.<Integer, Register>builder()
+			.put(0, Register.BC).put(1, Register.DE).put(2, Register.IX).put(3, Register.SP)
+			.build();
 
-		Map<Integer, Register> qq = new HashMap<Integer, Register>();
-		qq.put(0, Register.BC);
-		qq.put(1, Register.DE);
-		qq.put(2, Register.HL);
-		qq.put(3, Register.AF);
-		map.put(RegisterType.qq, qq);
+		Map<Integer, Register> rr = ImmutableMap.<Integer, Register>builder()
+			.put(0, Register.BC).put(1, Register.DE).put(2, Register.IY).put(3, Register.SP)
+			.build();
+		
+		registerMap = ImmutableMap.<Decoder.RegisterType, Map<Integer, Register>> builder()
+			.put(RegisterType.r, r).put(RegisterType.dd, dd).put(RegisterType.ss, dd)
+			.put(RegisterType.qq, qq).put(RegisterType.pp, pp).put(RegisterType.rr, rr)
+			.build();
 
-		Map<Integer, Register> pp = new HashMap<Integer, Register>();
-		pp.put(0, Register.BC);
-		pp.put(1, Register.DE);
-		pp.put(2, Register.IX);
-		pp.put(3, Register.SP);
-		map.put(RegisterType.pp, pp);
-
-		Map<Integer, Register> rr = new HashMap<Integer, Register>();
-		rr.put(0, Register.BC);
-		rr.put(1, Register.DE);
-		rr.put(2, Register.IY);
-		rr.put(3, Register.SP);
-		map.put(RegisterType.rr, rr);
-
-		flagMap = new HashMap<Integer, Flag>();
-		flagMap.put(0, Flag.Z);
-		flagMap.put(1, Flag.C);
-		flagMap.put(2, Flag.PV);
-		flagMap.put(3, Flag.S);
+		flagMap = ImmutableMap.<Integer, Flag> builder()
+			.put(0, Flag.Z).put(1, Flag.C).put(2, Flag.PV).put(3, Flag.S)
+			.build();
 	}
 
 	public Register decodeRegister(final RegisterType type, final int opCode) {
@@ -78,7 +61,7 @@ public class Decoder {
 	}
 
 	private Register decode(final RegisterType type, final int code) {
-		return map.get(type).get(code);
+		return registerMap.get(type).get(code);
 	}
 
 	private int extractHigherRegisterCode(final int opCode) {
