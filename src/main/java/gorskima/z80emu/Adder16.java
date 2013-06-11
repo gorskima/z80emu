0026993b3863fcd3b033115c936a0ce070b74211
Mariusz Gorski <marius.gorski@gmail.com>
2013-06-11 23:32:52 +0200

    Implement Adder16 with add, carry and halfCarry methods

---
+++ src/main/java/gorskima/z80emu/Adder16.java
@@ -0,0 +1,31 @@
+package gorskima.z80emu;
+
+public class Adder16 {
+
+	private final static int SIZE = 16;
+	private boolean[] carries = new boolean[SIZE];
+
+	public int add(final int op1, final int op2, final int carryIn) {
+		int result = 0;
+		int carry = carryIn;
+
+		for (int shift = 0; shift < SIZE; shift++) {
+			int a = (op1 >> shift) & 1;
+			int b = (op2 >> shift) & 1;
+			int r = a ^ b ^ carry;
+			result = r == 1 ? result | 1 << shift : result & ~(1 << shift);
+			carry = (a & b) | (carry & (a ^ b));
+			carries[shift] = carry == 1;
+		}
+		return result;
+	}
+
+	public boolean isCarry() {
+		return carries[15];
+	}
+
+	public boolean isHalfCarry() {
+		return carries[11];
+	}
+
+}
---
+++ src/test/java/gorskima/z80emu/Adder16Test.java
@@ -0,0 +1,51 @@
+package gorskima.z80emu;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Adder16Test {
+
+	@Test
+	public void testAddWithCarry() {
+		addWithCarry(0, 0, 0, 0, 0);
+		addWithCarry(1, 0, 0, 1, 0);
+		addWithCarry(1, 0, 1, 2, 0);
+		addWithCarry(1, 1, 1, 3, 0);
+		
+		addWithCarry(65535, 0, 0, 65535, 0);
+		addWithCarry(65535, 1, 0, 0, 1);
+		addWithCarry(65535, 0, 1, 0, 1);
+		addWithCarry(65535, 1, 1, 1, 1);
+		
+		addWithCarry(65535, 65535, 1, 65535, 1);
+	}
+	
+	@Test
+	public void testAddWithHalfCarry() {
+		addWithHalfCarry(4095, 0, 0, 4095, 0);
+		addWithHalfCarry(4095, 1, 0, 4096, 1);
+		addWithHalfCarry(4095, 0, 1, 4096, 1);
+		addWithHalfCarry(4095, 1, 1, 4097, 1);
+	}
+
+	private void addWithCarry(final int op1, final int op2, final int carry, final int expectedResult,
+			final int expectedCarry) {
+		
+		Adder16 adder = new Adder16();
+		int result = adder.add(op1, op2, carry);
+		assertThat(result, is(expectedResult));
+		assertThat(adder.isCarry(), is(expectedCarry == 1));
+	}
+	
+	private void addWithHalfCarry(final int op1, final int op2, final int carry, final int expectedResult,
+			final int expectedCarry) {
+		
+		Adder16 adder = new Adder16();
+		int result = adder.add(op1, op2, carry);
+		assertThat(result, is(expectedResult));
+		assertThat(adder.isHalfCarry(), is(expectedCarry == 1));
+	}
+
+}
